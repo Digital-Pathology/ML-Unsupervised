ee103917afad6fe63e1599f277222af2f784f4f8
Adin Solomon <adinbsolomon@gmail.com>
2022-03-25 00:40:15 +0000

    mid maskrcnn data conversions

--- Datasets/MoNuSeg/FileReader.py
+++ Datasets/MoNuSeg/FileReader.py
@@ -1,21 +1,24 @@
 
 import os
 
-
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import torch
 from torchvision import transforms
 
+import xmltodict
+
 # filepaths and extensions
 path_data_dir = __file__.replace(os.path.basename(__file__),"")
 path_data_images = path_data_dir + 'Images/'
 path_data_masks = path_data_dir + 'Masks/'
 path_data_colormasks = path_data_dir + 'ColorMasks/'
+path_data_annotations = path_data_dir + 'Annotations/'
 path_extension_image = '.png'
 path_extension_mask = '-mask.png'
 path_extension_colormask = '-mask-color.png'
+path_extension_annotation = '.xml'
 
 # get a list of the filenames
 files = [f.replace(path_extension_image,'') for f in os.listdir(path_data_images)]
@@ -42,3 +45,15 @@ def get_colormask(filename):
     colormask = Image.open(filepath)
     colormask = pil_to_tensor(colormask)
     return colormask
+def get_annotations(filename):
+    filepath = clean_filepath(filename, path_data_annotations, path_extension_annotation)
+    annotations = None
+    with open(filepath, 'r') as f:
+        annotations = f.read()
+        annotations = xmltodict.parse(annotations)
+    return annotations
+
+def process_annotations(annotations):
+    regions = []
+    regions_iter = annotations['Annotations']['Annotation']['Regions'].items()
+
