84df8e0388eba9a17bfbd5562f29bc1e716312bb
Adin Solomon <adinbsolomon@gmail.com>
2022-04-03 16:52:40 +0000

    updated model manager

--- Segmentation/Utils.py
+++
@@ -1,8 +0,0 @@
-
-# https://stackoverflow.com/a/46336730
-def bounding_box(points):
-    x_coordinates, y_coordinates = zip(*points)
-    return [
-        (min(x_coordinates), min(y_coordinates)),
-        (max(x_coordinates), max(y_coordinates))
-    ]
