36f9724f136f36ad6dd680975a80871dd8a68fdc
Adin Solomon <adinbsolomon@gmail.com>
2022-05-06 18:12:32 +0000

    for copying, building, and pushing efficiently

--- sagemaker-template.sh
+++ sagemaker-template.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-copy .
+date
+
+# copy material over from this directory
+time copy .
+
+# remove the big unnecessary files
 cd /workspaces/dev-container/SagemakerTemplate/code
 rm -rf \
+    .devcontainer \
     .git \
-    databases \
-    testing
-build_and_push adin_model_eval_test
+    datasets \
+    testing \
+    *.ipynb \
+    filtration_cache.h5 \
+    README.md \
+    model_analysis_results_weight_ratio.csv \
+    region_predictions_aggregated.json
+
+# do the thing
+time build_and_push adin_model_eval_test
+
+date
