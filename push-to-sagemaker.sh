d6c506e61288a8e2b097534b8072c02eb103acbf
Adin Solomon <adinbsolomon@gmail.com>
2022-05-08 05:58:42 +0000

    rename for better autocomplete

--- push-to-sagemaker.sh
+++ push-to-sagemaker.sh
@@ -9,14 +9,18 @@ time copy .
 cd /workspaces/dev-container/SagemakerTemplate/code
 rm -rf \
     .devcontainer \
-    .git \
+    .git* \
+    archive \
     datasets \
     testing \
     *.ipynb \
     filtration_cache.h5 \
     README.md \
     model_analysis_results_weight_ratio.csv \
-    region_predictions_aggregated.json
+    region_predictions_aggregated.json \
+    output \
+    sagemaker-template.sh \
+    environment.yml
 
 # do the thing
 time build_and_push adin_model_eval_test
